c8963d7cfb2cd031a7ffbbc86729c5f028ca2e68
Mohamed Sherif <Medielninja@outlook.com>
2024-08-01 16:30:39 +0300

    feat: add search and pagination.

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,12 @@
-export default function Page() {
-    return <p>Customers Page</p>;
-  }
-  
+import { fetchCardData } from "@/app/lib/data";
+import { Card } from "@/app/ui/dashboard/cards";
+
+export default async function Page() {
+  const {totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData(); 
+  return (
+    <main>
+      <p>Customers Page</p>
+      <Card title="Customers" value={numberOfCustomers} type="customers"></Card>
+    </main>
+  );
+}
