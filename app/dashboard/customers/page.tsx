a47f496b43c4e5f5f1da54de8208c214171293e6
Mohamed Sherif <Medielninja@outlook.com>
2024-08-05 18:04:54 +0300

    Added login and signout

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -2,11 +2,9 @@ import { fetchCardData } from "@/app/lib/data";
 import { Card } from "@/app/ui/dashboard/cards";
 
 export default async function Page() {
-  const {totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData(); 
   return (
     <main>
       <p>Customers Page</p>
-      <Card title="Customers" value={numberOfCustomers} type="customers"></Card>
     </main>
   );
 }
